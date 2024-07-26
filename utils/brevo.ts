3512fdee7044a7031829ed2b0de508234ba72fed
2108gamer <ricarlopez2708@gmail.com>
2024-07-25 20:37:09 -0600

    Descripción del commit

--- utils/brevo.ts
+++ utils/brevo.ts
@@ -5,7 +5,7 @@ const emailSend = new TransactionalEmailsApi()
 
 emailSend.setApiKey(
     TransactionalEmailsApiApiKeys.apiKey,
-   "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" 
+   "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" 
 )
 
 const smtpEmail = new SendSmtpEmail()
@@ -13,9 +13,10 @@ const smtpEmail = new SendSmtpEmail()
 
 export async function sendEmail() {
 try {
+    
     smtpEmail.subject = "Copia de Acta"
-    smtpEmail.to = [{email: "ricarlopez2708@gmail.com", name: "Ricardo"}]
-    smtpEmail.htmlContent = "<h1>hola</h1>"
+    smtpEmail.to = [{email: "ricarlopez2708@gmail.com", name: "Ricardo"}, {email: "esligil005@gmail.com", name: "Eshli"}]
+    smtpEmail.templateId = 1
     smtpEmail.sender = {name: "Eshli", email: "panel@strxnge.us"}
     
     emailSend.sendTransacEmail(smtpEmail)
